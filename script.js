8c11719c2a62c5e66de9e37b741fe03340533b50
Webdevsonu <108911910+Webdevsonu@users.noreply.github.com>
2023-01-05 18:06:01 +0530

    FAQ card

---
+++ script.js
@@ -0,0 +1,45 @@
+document.getElementById("down-arrow").onclick = function() {
+	document.getElementById("answer1").style.display = "block";
+	document.getElementById("answer2").style.display = "none";
+	document.getElementById("answer3").style.display = "none";
+	document.getElementById("answer4").style.display = "none";
+	document.getElementById("answer5").style.display = "none";
+	document.getElementById("question1").style.fontWeight = "bold";
+}
+
+document.getElementById("down-arrow2").onclick = function() {
+	document.getElementById("answer2").style.display = "block";
+	document.getElementById("answer1").style.display = "none";
+	document.getElementById("answer3").style.display = "none";
+	document.getElementById("answer4").style.display = "none";
+	document.getElementById("answer5").style.display = "none";
+	document.getElementById("question2").style.fontWeight = "bold";
+}
+
+document.getElementById("down-arrow3").onclick = function() {
+	document.getElementById("answer3").style.display = "block";
+	document.getElementById("answer2").style.display = "none";
+	document.getElementById("answer1").style.display = "none";
+	document.getElementById("answer4").style.display = "none";
+	document.getElementById("answer5").style.display = "none";
+	document.getElementById("question3").style.fontWeight = "bold";
+}
+
+document.getElementById("down-arrow4").onclick = function() {
+	document.getElementById("answer4").style.display = "block";
+	document.getElementById("answer2").style.display = "none";
+	document.getElementById("answer3").style.display = "none";
+	document.getElementById("answer5").style.display = "none";
+	document.getElementById("answer1").style.display = "none";
+	document.getElementById("question4").style.fontWeight = "bold";
+}
+
+document.getElementById("down-arrow5").onclick = function() {
+	document.getElementById("answer5").style.display = "block";
+	document.getElementById("answer4").style.display = "none";
+	document.getElementById("answer2").style.display = "none";
+	document.getElementById("answer3").style.display = "none";
+	document.getElementById("answer1").style.display = "none";
+	document.getElementById("question5").style.fontWeight = "bold";
+}
+
